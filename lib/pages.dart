d54431453593243a24ff28177fe17a87b70f4feb
jose-oyuko <oyukojoseph13@gmail.com>
2025-01-31 00:15:45 +0300

    add navbar and pages

---
+++ lib/pages.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DashBoardPage extends StatelessWidget {
+  const DashBoardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dash Board Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Register Customer Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
